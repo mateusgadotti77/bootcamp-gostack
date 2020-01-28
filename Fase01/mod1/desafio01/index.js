f51c15b1c44a05b0227e5c8ae458cb29a7251555
Gadotti <maateusgadotti@gmail.com>
2020-01-28 13:16:17 -0300

    Finished module01

---
+++ Fase01/mod1/desafio01/index.js
@@ -0,0 +1,79 @@
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
+
+const projects = [];
+
+function checkIDExists(req, res, next) {
+  const { id } = req.params;
+
+  const project = projects.find(p => p.id == id);
+
+  if (!project) {
+    return res.status(400).json({ error: 'Project does not exists!' })
+  } 
+
+  return next();
+}
+
+server.use((req, res, next) => {
+
+  console.count('Número de Requisições');
+
+  return next();
+}) 
+
+
+server.post('/projects', (req, res) => {
+  const { id, title } = req.body;
+
+  const project = {
+    id,
+    title,
+    tasks: []
+  };
+
+  projects.push(project);
+
+  return res.json(project)
+});
+
+server.get('/projects', (req, res) => {
+  return res.json(projects)
+});
+
+server.put('/projects/:id', checkIDExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.title = title;
+
+  return res.json(project)
+});
+
+server.delete('/projects/:id', checkIDExists, (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.findIndex(p => p.id == id);
+
+  projects.splice(projectIndex, 1);
+
+  return res.send();
+});
+
+server.post('/projects/:id/tasks', checkIDExists, (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+
+  const project = projects.find(p => p.id == id);
+
+  project.tasks.push(title);
+
+  return res.json(project);
+});
+
+server.listen(3000);
---
+++ README.md
@@ -0,0 +1,19 @@
+<h1 align="center">
+  Bootcamp GoStack
+</h1>
+
+<h1 align="center">
+    <img alt="GoStack" src="https://i.imgur.com/WfafA3y.png" width="200px" />
+</h1>
+
+<p align="center"> Desenvolvimentos referentes ao Bootcamp GoStack da Rocketseat 🚀.
+
+
+## Features
+
+- **Node.js** -   Framework que permite utilizar javascript tanto no frontend quanto no backend
+
+
+## Desenvolvimentos
+
+- **Fase 01 - Ambientes e conceitos + Desafio 01** - Neste primeiro módulo da primeira fase, foram iniciados os conceitos básicos do Node.js, foram utilizados o *Express*, o *Nodemon* e o *Insomnia* para testes das requisições, foram também abordados os conceitos de *GET*/*PUT*/*POST*/*DELETE* e *Middlewares*
