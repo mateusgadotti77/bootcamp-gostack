76ffb4dbe441b1fdd8773863a67988e753e1b7bf
Gadotti <maateusgadotti@gmail.com>
2020-01-28 21:53:28 -0300

    Config ESLint, Prettier, EditorConfig & Sequelize

--- Fase01/mod2/modulo02/src/routes.js
+++ Fase01/mod2/modulo02/src/routes.js
@@ -2,8 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req,  res) => {
-  return res.json({ messsage: 'Hello Omni!' })
-})
+routes.get('/', (req, res) => res.json({ messsage: 'Hello Omni!' }));
 
 export default routes;
