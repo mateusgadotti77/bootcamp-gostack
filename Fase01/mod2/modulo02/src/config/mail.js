77732c08d8254087af657184f9f3011bd5c2da30
Gadotti <maateusgadotti@gmail.com>
2020-02-03 07:42:45 -0300

    Notifications, appointment canceled and send email

---
+++ Fase01/mod2/modulo02/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'a6d3bc54f478f6',
+    pass: 'fd9a89913419f5',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreplay@gobarber.com>',
+  },
+};
---
+++ Fase01/mod2/modulo02/src/lib/Mail.js
@@ -0,0 +1,28 @@
+import nodemailer from 'nodemailer';
+import { resolve } from 'path';
+import exphbs from 'express-handlebars';
+import nodemailerhbs from 'nodemailer-express-handlebars';
+
+import mailConfig from '../config/mail';
+
+class Mail {
+  constructor() {
+    const { host, port, secure, auth } = mailConfig;
+
+    this.tranporter = nodemailer.createTransport({
+      host,
+      port,
+      secure,
+      auth: auth.user ? auth : null,
+    });
+  }
+
+  sendMail(message) {
+    return this.tranporter.sendMail({
+      ...mailConfig.default,
+      ...message,
+    });
+  }
+}
+
+export default new Mail();
